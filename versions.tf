3a3135bc4cef91abb587db25dd3c29f97586fc4d
Kkhandale2024 <kalyani.khandale@clouddrove.com>
2024-06-20 18:03:06 +0530

    feat: updated readme.yml

--- versions.tf
+++ versions.tf
@@ -2,11 +2,12 @@
 # Versions
 # ------------------------------------------------------------------------------
 terraform {
-  required_version = ">= 1.4.6"
+  required_version = ">= 1.5.4"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.1.0"
+      version = ">= 5.11.0"
     }
   }
 }
