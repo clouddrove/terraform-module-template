d7af6cacdbe172b474bc4ebdd0e6553aa4d9adc8
Kkhandale2024 <155427611+Kkhandale2024@users.noreply.github.com>
2025-04-02 19:53:45 +0530

    Feat/ Terraform Template Module (#24)
    * feat: updated workflows and readme
    
    * feat: updated readme
    
    * feat: done changes in readme and readme.yml
    
    * feat: updated readme.yml
    
    * feat: done changes in tfcheck file
    
    * feat: resolvedissue of tfcheck file
    
    * feat: changes in tfcheck file
    
    * feat: removed some line from readme.yml
    
    * feat: changed providerin readme.yml
    
    * feat: update version and link in readme
    
    * feat: updated version in readme.yml
    
    * feat: replace module name in readme.yml
    
    * feat: updated uses tag
    
    * feat: used latest tags for workflow uses
    
    * feat: used latest tags for workflow uses
    
    * feat: used master branch for workflow uses
    
    * feat: updated readme.md
    
    ---------
    
    Co-authored-by: Vishwajit Nagulkar <119565952+VishwajitNagulkar@users.noreply.github.com>

--- versions.tf
+++ versions.tf
@@ -2,11 +2,12 @@
 # Versions
 # ------------------------------------------------------------------------------
 terraform {
-  required_version = ">= 1.4.6"
+  required_version = ">= 1.10.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.1.0"
+      version = ">= 5.55.0"
     }
   }
 }
